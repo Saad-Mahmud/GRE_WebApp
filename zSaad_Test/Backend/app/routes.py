d3770ff106cb67201c34e4996e4f33929a2d53b0
saadmahmud <saadmahmud14@gmail.com>
2018-10-21 21:36:16 +0600

    Update

--- zSaad_Test/Backend/app/routes.py
+++ zSaad_Test/Backend/app/routes.py
@@ -9,10 +9,21 @@ def hello_world():
     return render_template("dummy.html")
 
 
-@APP_MAIN.route('/dictionary')
-def dictionary():
-    wordlist=Words_Test.objects(wordID__startswith ='a_')
-    print(wordlist[0].wordID)
+@APP_MAIN.route('/dictionary/',defaults={'page': 'ALL'})
+@APP_MAIN.route('/dictionary/<string:page>')
+def dictionary(page):
+    page = page.lower()
+    title = page.capitalize()
+    links = ['All','A','B','C','D','E','F','G'
+        ,'H','I','J','K','L','M','N','O','P',
+             'Q','R','S','T','U','V','W','X','Y','Z']
+    print(title)
+    if(page=='all'):
+        wordlist=Words_Test.objects
+    elif(page[0]>='a' and page[0]<='z' and len(page)==1):
+        wordlist = Words_Test.objects(wordID__startswith=page[0])
+    else:
+        return render_template('404.html')
     words = [
         {
             'word': w.word ,
@@ -22,5 +33,5 @@ def dictionary():
         }
         for w in wordlist
     ]
-    return render_template('dictionary.html', words=words)
+    return render_template('dictionary.html',links=links, title=title, words=words)
 
