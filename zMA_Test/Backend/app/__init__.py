723cd9ba1551a5b3cd7f7050a59e479ec379b522
saadmahmud <saadmahmud14@gmail.com>
2018-10-21 14:22:29 +0600

    Fixing Mou

--- zMA_Test/Backend/app/__init__.py
+++ zMA_Test/Backend/app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from zMA_Test.Backend.config import Config
 from mongoengine import connect
 import pymongo
-from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 import os
 
@@ -17,8 +16,7 @@ APP_MAIN.config.from_object(Config.Config)
 APPBS=Bootstrap(APP_MAIN)
 client = pymongo.MongoClient("localhost", 27017)
 db = client.tutorial_mega
-connect('tutorial_mega', host='127.0.0.1', port=27017)
-APPLOGIN = LoginManager(APP_MAIN)
-APPLOGIN.login_view = 'login'
+connect('zSaad_Test', host='127.0.0.1', port=27017)
+
 
 from zMA_Test.Backend.app import routes, model
--- zMA_Test/Backend/practice/fetch_practice.py
+++ zMA_Test/Backend/practice/fetch_practice.py
@@ -3,14 +3,11 @@ import pymongo
 from zSaad_Test.Backend.initDB.words import Words_Rating
 
 
-client = pymongo.MongoClient("localhost", 27017)
-db = client.zSaad_Test #DBname
-connect('zSaad_Test', host='127.0.0.1', port=27017)
-
 
 def fetch_easy():
     print("I came in fetch_easy", Words_Rating.objects.__sizeof__())
     a = Words_Rating.objects
+    print(a)
     for _ in a:
         print("hiiiiiiiiiiiiiiiiiiii")
         print(_.wordID)
