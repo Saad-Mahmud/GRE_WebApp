1e9c6c7b74517d5d640c936203bde354e4b00abb
saadmahmud <saadmahmud14@gmail.com>
2018-10-21 14:11:19 +0600

    All 1000 words

--- zMA_Test/Backend/testdb.py
+++ zMA_Test/Backend/testdb.py
@@ -1,6 +1,6 @@
 from mongoengine import *
 import pymongo
-
+'''
 client = pymongo.MongoClient("localhost", 27017)
 db = client.zMou_Test #DBname
 connect('zMou_Test', host='127.0.0.1', port=27017)
@@ -26,5 +26,7 @@ class Words_Test(Document):
     TYPE= StringField(required=True)
 
 init_DB_with_words()
+
+'''
 
 
