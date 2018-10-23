b9bfcf63f8fd5c11a220622d6039ea9d5c75dac6
moumita.ch <2015-516-794@student.cse.du.ac.bd>
2018-10-24 00:34:59 +0600

    practice basic done

--- zMA_Test/Backend/app/routes.py
+++ zMA_Test/Backend/app/routes.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from zMA_Test.Backend.app import APP_MAIN
 from flask import render_template, redirect,url_for,flash,send_file,request,json
 
@@ -13,9 +15,8 @@ def hello_world():
 @APP_MAIN.route('/practice')
 def practice():
     words = fetch_easy_words()
-    sessionID = create_session_practice(words,0)
-    print("session mmm {}".format(sessionID.id))
-    print("words ", words[0]);
+    status = {word['wordID']:'red' for word in words}
+    sessionID = create_session_practice(status, words, 0)
 
     return render_template('tryit.html', word=words[0], sessionID=sessionID.id)
 
@@ -24,30 +25,68 @@ def practice():
 def test():
     return render_template('test.html')
 
-@APP_MAIN.route('/clicked', methods=['POST'])
+
+@APP_MAIN.route('/fliped', methods=['POST'])
 def translate():
     sessionID = request.form['sessionID']
     pointer_f = session_practice.objects(id=sessionID)[0]
-    pointer = pointer_f.idx
-    words = pointer_f.words
 
-    print("pointer: " + str(pointer))
-    word = words[pointer]
+    pointer = pointer_f.idx # fetched pointer
+    words = pointer_f.words # fetched word list
+
+    if len(words)!=0:
+        word = words[pointer]
+    else:
+        word = {'wordID':"$null$"}
 
     return json.dumps({'word': word})
 
 @APP_MAIN.route('/nextword', methods=['POST'])
 def nextWord():
     sessionID = request.form['sessionID']
+    buttonID = request.form['buttonID']
     pointer_f = session_practice.objects(id=sessionID)[0]
-    pointer = pointer_f.idx + 1
-    words = pointer_f.words
-    pointer_f.idx = pointer
-    pointer_f.save()
-    print("pointer: " + str(pointer))
-    word = words[pointer]
 
-    return json.dumps({'word': word})
+    pointer = pointer_f.idx # fetched, incremented pointer, saved the incremented pointer
+    words = pointer_f.words
+    status = pointer_f.status
+    currentWord = words[pointer]
+    currentWordID = currentWord['wordID']
+
+    if buttonID=='ik':
+        if status[currentWordID]=='red':
+            status[currentWordID] = 'yellow'
+
+            words.remove(currentWord)
+            rand = randint(0, len(words))
+            # print("randint ", rand)
+            words.insert(rand,currentWord)
+
+        elif status[currentWordID]=='yellow':
+            status[currentWordID] = 'green'
+            words.remove(currentWord)
+
+    elif buttonID=='idk':
+        if status[currentWordID]=='yellow':
+            status[currentWordID]='yellow'
+
+            words.remove(currentWord)
+            rand = randint(0, len(words))
+            # print("blaint ", rand)
+            words.insert(rand, currentWord)
+
+    if len(words) != 0:
+        pointer = (pointer + 1) % len(words)
+        newWord = words[pointer]
+        pointer_f.words = words
+        pointer_f.status = status
+        pointer_f.idx = pointer
+        pointer_f.save()
+    else:
+        pointer = -1
+        newWord = {'wordID': '$null$'}
+
+    return json.dumps({'word': newWord})
 
 @APP_MAIN.route('/tryit')
 def tryit():
--- zMA_Test/Backend/practice/fetch_practice.py
+++ zMA_Test/Backend/practice/fetch_practice.py
@@ -3,44 +3,98 @@ import operator
 from zMA_Test.Backend.app.model import session_practice
 from zSaad_Test.Backend.initDB.words import Words_Rating, Words_Test
 
-
+def takeSecond(elem, country_id):
+    return elem[1][country_id]
 def fetch_easy_rating(country_id=0):
-    print("I came in fetch_easy", Words_Rating.objects.__sizeof__())
+    #print("I came in fetch_easy", Words_Rating.objects.__sizeof__())
+
+    """
+
+    :param country_id:
+    :return:
+
+
+    temp_list = Words_Test.objects()
+
+    temps = [
+        {
+            'wordID': w.wordID,
+            'word': w.word,
+            'TYPE': w.TYPE,
+            'meaning': w.meanings[0],
+            'usages': w.usages
+        }
+        for w in temp_list
+    ]
+    for w in temps:
+        print("words ",w['wordID'])
+    """
+
+    temp_list = Words_Rating.objects()
+
+    temps = [
+        {
+            'wordID': w.wordID,
+            'Ratings': w.Ratings,
+
+        }
+        for w in temp_list
+    ]
+
+
+    sorted_temp = sorted(temps, key=lambda k: k['Ratings'][country_id])
+    """
+    for w in sorted_temp:
+        print("words ", w['wordID'], w['Ratings'][country_id])
+    
     word_list = Words_Rating.objects()
     word_dict = {}
+    
     for word in word_list:
         word_dict[word.wordID] = word.Ratings[country_id]
+        
         #print("Titiiiiiiiiiiiiiiiiiiiiii : ", word.wordID, word.Ratings[0])
     sorted_dict = sorted(word_dict.items(), key=operator.itemgetter(1))
 
+    
     for id, ratings in sorted_dict[1:5]:
         print("Now see if sort : ", id, ratings)
     #print("Hi there save me : ", sorted_dict[1:5])
+    """
 
-    return sorted_dict[1:5]
+    return sorted_temp[1:5]
 
 
 def fetch_easy_words(country_id=0):
-    final_easy_list = []
+
+    final_list = []
     rated_words = fetch_easy_rating(country_id)
     words_dict = Words_Test.objects()
 
     for w1 in rated_words:
         for w2 in words_dict:
-            if w1[0] == w2.wordID:
+            if w1['wordID'] == w2.wordID:
                 print("meaningggggg : ", w2.word, w2.meanings)
-                final_easy_list.append([w2.wordID, w2.word, w2.meanings, w2.usages, w2.translations, w2.TYPE ])
+                #final_easy_list.append([w2.wordID, w2.word, w2.meanings, w2.usages, w2.translations, w2.TYPE ])
+                final_list.append(
+                    {
+                        'wordID': w2.wordID,
+                        'word': w2.word,
+                        'TYPE': w2.TYPE,
+                        'meaning': w2.meanings[0],
+                        'usages': w2.usages
+                    })
 
     """
-    for i in final_easy_list:
-        print(i.wordID, i.word, i.meanings)
-    print("helllooooooooooooooooo ", final_easy_list)
+        for i in final_list:
+        print("final list ",i['wordID'], i['word'], i['meaning'])
     """
 
-    return final_easy_list
+
+    return final_list
 
 
-def create_session_practice(words,idx):
-    session= session_practice(words=words,idx=idx)
+def create_session_practice(status,words,idx):
+    session= session_practice(status=status, words=words, idx=idx)
     session2 = session.save()
     return session2
