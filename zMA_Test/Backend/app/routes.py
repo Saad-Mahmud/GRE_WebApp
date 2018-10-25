d1079c369d1bb30b92238e2c9855052b639b8496
amit-099 <amitcsedu99@gmail.com>
2018-10-25 18:09:05 +0600

    test updated

--- zMA_Test/Backend/app/routes.py
+++ zMA_Test/Backend/app/routes.py
@@ -4,14 +4,21 @@ import operator
 import random
 import unicodedata
 
+from flask import render_template, request, json
+from flask_login import login_required, current_user
 from flask import render_template, request, json,send_file
 
+from zKM_Test.Backend.app.model import Gre_data
 from zMA_Test.Backend.app import APP_MAIN
+from zMA_Test.Backend.app.model import session_practice, session_test
+from zMA_Test.Backend.practice.fetch_practice import fetch_easy_words, create_session_practice
+from zMA_Test.Backend.test.fetch_test import fetch_easy_words2, create_session_test
 from zMA_Test.Backend.app.model import session_practice, session_test, user_word_history
 from zMA_Test.Backend.practice.fetch_practice import fetch_easy_words, create_session_practice, \
     create_user_word_history, update_user_word_status
 from zMA_Test.Backend.practice.practice_util import showstat
 from zMA_Test.Backend.test.fetch_test import fetch_easy_words2, create_session_test
+from zMA_Test.Backend.test.test_util import show_test_stat
 from zSaad_Test.Backend.initDB.words import Words_Test
 
 
@@ -20,58 +27,178 @@ def hello_world():
     return render_template("dummy.html")
 
 
+
 @APP_MAIN.route('/test')
 def test():
+    #username = unicodedata.normalize('NFKD', current_user.username).encode('ascii', 'ignore')
+    #print(type(username))
     test_words = fetch_easy_words2()
-    sessionID = create_session_test(test_words, 0)
-    print("session mmm {}".format(sessionID.id))
-    #option_words = fetch_easy_words2()
-    #print(test_words[1][1])
-    random_idx = random.sample(range(1, 10), 3)
-    #print("rand idx", random_idx)
-    option = []
-    option.append(test_words[0][1])
+    status = {}
+    sessionID = create_session_test(status, test_words, 0)
+    temp = Gre_data.objects(username="amit99")[0]
 
-    #print(option_words[9][1])
+    print("session mmm {}".format(sessionID.id))
+    print(type(test_words))
+    random_idx = random.sample(range(1, 10), 3)
+    option = []
+    option_multiple_choice = []
+
+    option.append(test_words[0][1])
+    option_multiple_choice.append(test_words[0][2][0])
+    print('amiiiiiiii', test_words[0][2][0])
+
+    # print("aaaaaaaaaaaaaa", type(test_words[0][1]))
     for i in range(3):
         option.append(test_words[random_idx[i]][1])
-    #print("options", option)
+        option_multiple_choice.append(test_words[random_idx[i]][2][0])
 
     random_idx2 = random.sample(range(1, 5), 4)
-    #print(random_idx2)
     option_dict = {}
+    option_multiple_choice_dict = {}
+
     for i in range(4):
-        option_dict[unicodedata.normalize('NFKD', option[i]).encode('ascii','ignore')] = random_idx2[i]
-    test_line =  unicodedata.normalize('NFKD', test_words[0][3][0]).encode('ascii','ignore')
-    #print(type(test_line))
-    #print("eeee", test_line)
+        option_dict[unicodedata.normalize('NFKD', option[i]).encode('ascii', 'ignore')] = random_idx2[i]
+        option_multiple_choice_dict[
+            unicodedata.normalize('NFKD', option_multiple_choice[i]).encode('ascii', 'ignore')] = random_idx2[i]
+
+    test_multi_choice_word = ''
+    test_line = unicodedata.normalize('NFKD', test_words[0][3][0]).encode('ascii', 'ignore')
+    test_multi_choice_word += 'Meaning of '
+    choice_word = unicodedata.normalize('NFKD', test_words[0][1]).encode('ascii', 'ignore')
+    choice_word = choice_word.upper()
+    test_multi_choice_word += choice_word
+
+    print('multichoice', test_multi_choice_word)
+    print(type(test_multi_choice_word))
 
     sorted_dict = sorted(option_dict.items(), key=operator.itemgetter(1))
+    sorted_multi_choice_dict = sorted(option_multiple_choice_dict.items(), key=operator.itemgetter(1))
 
-    #print(type(sorted_dict))
-    #print(sorted_dict)
+    print('#######################')
+    print(type(sorted_multi_choice_dict))
+    print(type(sorted_dict))
+    print('#######################')
 
-    test_line = test_line.replace((unicodedata.normalize('NFKD', test_words[0][1]).encode('ascii','ignore')), "___")
+    test_line = test_line.replace((unicodedata.normalize('NFKD', test_words[0][1]).encode('ascii', 'ignore')), "___")
 
-    #print("eeeeeeeeeeeeeeeee", test_line)
+    ans_blanks = unicodedata.normalize('NFKD', test_words[0][1]).encode('ascii', 'ignore')
+    ans_multi = unicodedata.normalize('NFKD', test_words[0][2][0]).encode('ascii', 'ignore')
 
-    #print(type(option_dict))
-    #print(option_dict)
-    return render_template('test_new.html', test_word=test_words[0], test_line=test_line, option_dict=sorted_dict, sessionID=sessionID.id)
+    print(ans_multi)
+
+    return render_template('test_new.html', test_word=test_words[0], test_line=test_line,
+                           multi_word=test_multi_choice_word, option_dict=sorted_dict,
+                           multi_dict=sorted_multi_choice_dict, sessionID=sessionID.id)
+
 
 @APP_MAIN.route('/nexttestword', methods=['POST'])
 def nextTestWord():
+    #username = unicodedata.normalize('NFKD', current_user.username).encode('ascii', 'ignore')
+    username = 'amit99'
     answer = request.form['answer']
     sessionID = request.form['sessionID']
+    isWhat = request.form['isWhat']
+
+    isWhat = unicodedata.normalize('NFKD', isWhat).encode('ascii', 'ignore')
+    print("pppppppppppppp", type(isWhat))
     pointer_f = session_test.objects(id=sessionID)[0]
     pointer = pointer_f.idx + 1
     test_words = pointer_f.words
-    print('Ansssssssssssssss', answer)
-    print('pointAnssssssssss', test_words[pointer_f.idx][1])
+
+    if isWhat == 'true':
+        pointer_f.status[test_words[pointer_f.idx][1]] = answer
+        print('GivenAnsssssssssssssss1', answer)
+        print('ActualAnssssssssssssss1', test_words[pointer_f.idx][1])
+    else:
+        pointer_f.status[test_words[pointer_f.idx][2][0]] = answer
+        print('GivenAnsssssssssssssss2', answer)
+        print('ActualAnssssssssssssss2', test_words[pointer_f.idx][2][0])
+
     pointer_f.idx = pointer
     pointer_f.save()
-    test_word = test_words[pointer]
-    return json.dumps({'test_word':test_word})
+
+    if pointer < len(test_words):
+        test_word = test_words[pointer]
+        option = []
+
+        if isWhat == 'true':
+            option.append(test_word[1])
+        else:
+            option.append(test_word[2][0])
+
+        if pointer <= 3:
+            random_idx1 = random.sample(range(0, pointer), 1)
+            random_idx2 = random.sample(range(pointer + 1, 10), 2)
+            random_idx = random_idx1 + random_idx2
+        elif pointer <= 7:
+            random_idx1 = random.sample(range(0, pointer), 2)
+            random_idx2 = random.sample(range(pointer + 1, 10), 1)
+            random_idx = random_idx1 + random_idx2
+        else:
+            random_idx = random.sample(range(0, pointer), 3)
+
+        for i in range(3):
+            if isWhat == 'true':
+                option.append(test_words[random_idx[i]][1])
+            else:
+                option.append(test_words[random_idx[i]][2][0])
+
+        random_idx_option = random.sample(range(1, 5), 4)
+        option_dict = {}
+
+        for i in range(4):
+            option_dict[unicodedata.normalize('NFKD', option[i]).encode('ascii', 'ignore')] = random_idx_option[i]
+
+        sorted_dict = sorted(option_dict.items(), key=operator.itemgetter(1))
+        correct, wrong = show_test_stat(pointer_f.status)
+        if isWhat == 'true':
+            test_line = unicodedata.normalize('NFKD', test_word[3][0]).encode('ascii', 'ignore')
+            test_line = test_line.replace((unicodedata.normalize('NFKD', test_word[1]).encode('ascii', 'ignore')),
+                                          "___")
+            return json.dumps({'test_word': test_word, 'test_line': test_line, 'option_dict': sorted_dict, 'correct': correct, 'wrong': wrong})
+        else:
+            test_multi_choice_word = ''
+            test_multi_choice_word += 'Meaning of '
+            choice_word = unicodedata.normalize('NFKD', test_word[1]).encode('ascii', 'ignore')
+            choice_word = choice_word.upper()
+            test_multi_choice_word += choice_word
+            return json.dumps({'test_word': test_word, 'test_line': test_multi_choice_word, 'option_dict': sorted_dict, 'correct': correct, 'wrong': wrong})
+
+    else:
+        test_word = ['$null$']
+        session_data = session_test.objects(id=sessionID)[0]
+        print("abcdefgh", session_data.status)
+        test_key = 'test' + sessionID
+        gre_test_words = {
+            test_key: session_data.status
+        }
+
+        gre_data = Gre_data.objects(username=username)[0]
+        print(gre_data.username)
+        gre_data.history[test_key] = session_data.status
+        gre_data.save()
+
+        print("Ppppppppppppppppppppp", gre_test_words)
+        return json.dumps({'test_word': test_word})
+
+
+@APP_MAIN.route('/thisans', methods=['POST'])
+def nextAns():
+    sessionID = request.form['sessionID']
+    isWhat = request.form['isWhat']
+    isWhat = unicodedata.normalize('NFKD', isWhat).encode('ascii', 'ignore')
+
+    pointer_f = session_test.objects(id=sessionID)[0]
+    test_words = pointer_f.words
+
+
+    if isWhat == 'true':
+        answer = test_words[pointer_f.idx][1]
+    else:
+        answer = test_words[pointer_f.idx][2][0]
+
+    return json.dumps({'ans': answer})
+
 
 
 @APP_MAIN.route('/practice')
---
+++ zMA_Test/Backend/test/test_util.py
@@ -0,0 +1,10 @@
+def show_test_stat(status):
+    correct = 0
+    wrong = 0
+
+    for s in status.items():
+        if s[1]==s[0] :
+            correct += 1
+        else:
+            wrong += 1
+    return  correct, wrong
