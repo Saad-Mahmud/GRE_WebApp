d54f34c40bee0632c37eabbf272b7be7945d1897
Koushik-Deb <koushik.imon@gmail.com>
2018-11-07 18:47:44 +0600

    minor minor minor

--- zKM_Test/Backend/app/dummy.py
+++ zKM_Test/Backend/app/dummy.py
@@ -14,7 +14,9 @@ client = pymongo.MongoClient("localhost",27017)
 db = client.zSaad_Test
 connect('zSaad_Test', host="127.0.0.1", port = 27017)
 
-cntttt = ["Bangladesh","America","England","Syria","Uganda","Honululu","India","Pakistan","Brazil","Argentina"]
+cntttt = ["Bangladesh","America","England","Syria","India","Pakistan","Brazil","Argentina", "India",
+          "Sri Lanka", "Nepal", "China", "Japan", "Russia", "Australia", "Other"
+          ]
 # pword = 'abc'
 # for i in range(11,16,1):
 #     x = random.randint(0,3)
